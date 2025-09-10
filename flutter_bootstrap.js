8c4fa41890eb5c0b3eca82f7a2ac019a1ef1ed31
barneysspeedshop <barneysspeedshop@users.noreply.github.com>
2025-09-10 08:39:09 +0000

    deploy: ab25294cca0e6ab9b8d9e69454d2ada87f162174

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "673759098"
+    serviceWorkerVersion: "1058413928"
   }
 });
