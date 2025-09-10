be4e37674fb8b29e4fc9ed1e55fb4ea514b1ea1f
Patrick Legacy <patrick@flexrentalsolutions.com>
2025-09-10 03:40:25 -0400

    dart format

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider(
-    create: (context) => TimelineViewModel()..init(context),
-    child: MaterialApp(
-      title: 'Legacy Timeline Scrubber Example',
-      theme: ThemeData(primarySwatch: Colors.blue, visualDensity: VisualDensity.adaptivePlatformDensity),
-      darkTheme: ThemeData.dark(),
-      home: const MyHomePage(),
-    ),
-  );
+        create: (context) => TimelineViewModel()..init(context),
+        child: MaterialApp(
+          title: 'Legacy Timeline Scrubber Example',
+          theme: ThemeData(primarySwatch: Colors.blue, visualDensity: VisualDensity.adaptivePlatformDensity),
+          darkTheme: ThemeData.dark(),
+          home: const MyHomePage(),
+        ),
+      );
 }
 
 class MyHomePage extends StatelessWidget {
@@ -27,38 +27,38 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Consumer<TimelineViewModel>(
-    builder: (context, viewModel, child) => Scaffold(
-      appBar: AppBar(
-        title: const Text('Legacy Timeline Scrubber Example'),
-        actions: [
-          Switch(
-            value: viewModel.useCustomTheme,
-            onChanged: (value) {
-              viewModel.setUseCustomTheme(value);
-            },
+        builder: (context, viewModel, child) => Scaffold(
+          appBar: AppBar(
+            title: const Text('Legacy Timeline Scrubber Example'),
+            actions: [
+              Switch(
+                value: viewModel.useCustomTheme,
+                onChanged: (value) {
+                  viewModel.setUseCustomTheme(value);
+                },
+              ),
+              const Padding(padding: EdgeInsets.all(8.0), child: Text('Use Custom Theme')),
+            ],
           ),
-          const Padding(padding: EdgeInsets.all(8.0), child: Text('Use Custom Theme')),
-        ],
-      ),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          Text('Visible Window: ${viewModel.visibleStartDate} - ${viewModel.visibleEndDate}'),
-          const SizedBox(height: 20),
-          SizedBox(
-            height: 100,
-            child: LegacyGanttTimelineScrubber(
-              totalStartDate: viewModel.totalStartDate,
-              totalEndDate: viewModel.totalEndDate,
-              visibleStartDate: viewModel.visibleStartDate,
-              visibleEndDate: viewModel.visibleEndDate,
-              onWindowChanged: viewModel.onWindowChanged,
-              tasks: viewModel.tasks,
-              theme: viewModel.theme,
-            ),
+          body: Column(
+            children: [
+              const SizedBox(height: 20),
+              Text('Visible Window: ${viewModel.visibleStartDate} - ${viewModel.visibleEndDate}'),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 100,
+                child: LegacyGanttTimelineScrubber(
+                  totalStartDate: viewModel.totalStartDate,
+                  totalEndDate: viewModel.totalEndDate,
+                  visibleStartDate: viewModel.visibleStartDate,
+                  visibleEndDate: viewModel.visibleEndDate,
+                  onWindowChanged: viewModel.onWindowChanged,
+                  tasks: viewModel.tasks,
+                  theme: viewModel.theme,
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    ),
-  );
+        ),
+      );
 }
